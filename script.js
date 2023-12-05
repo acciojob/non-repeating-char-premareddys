955e3c8883497b0e60f5dd80c8532781d22a44da
Siddareddygari Prema Reddy <92963246+premareddys@users.noreply.github.com>
2023-12-05 20:41:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let count = {};
+  
+  for (let ch of str) {
+    if (count[ch]) {
+      count[ch]++;
+    } else {
+    count[ch] = 1;
+    }
+  }
+
+  for (let ch in count) {
+    if (count[ch] === 1) {
+      return `  ${ch}  `;
+    }
+  }
+  return "null";
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
